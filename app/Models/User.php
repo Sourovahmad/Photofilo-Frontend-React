17c37487942a11fe991b952902fea05bf8084788
sourovahmad <mdsourovahmad19@gmail.com>
2021-11-30 16:26:52 +0600

    init and update the models

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function role_id()
+    {
+      return $this->belongsTo(role::class, 'role_id', 'id')->get();
+    }
+
 }
---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->insert([
+            [
+                "role" =>  "superadmin",
+            ],
+            [
+                "role" =>  "admin",
+            ],
+            [
+                "role" =>  "user",
+            ],
+        ]);
+
+        DB::table('users')->insert([
+            [
+                "name" =>  "Super Admin",
+                "email" =>  "superadmin@gmail.com",
+                "password" =>  bcrypt(1234),
+                "role_id" =>  1,
+            ],
+            [
+                "name" =>  "Admin",
+                "email" =>  "admin@gmail.com",
+                "password" =>  bcrypt(1234),
+                "role_id" =>  2,
+            ],
+            [
+                "name" =>  "user",
+                "email" =>  "user@gmail.com",
+                "password" =>  bcrypt(1234),
+                "role_id" =>  3,
+            ],
+        ]);
+    }
+}
